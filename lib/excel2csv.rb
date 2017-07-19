2c4505da5728a592dea3b5853b86b7acf222c1bb
Ken Eguchi <eguchi@haw.co.jp>
2017-07-19 13:42:04 +0900

    実行ファイル bin/excel2csv を追加

--- lib/excel2csv.rb
+++ lib/excel2csv.rb
@@ -1,5 +1,27 @@
 require "excel2csv/version"
+require "roo"
+require "csv"
 
 module Excel2csv
-  # Your code goes here...
+  class Main
+    def initialize(path)
+      @path = path
+    end
+
+    def validate
+      unless File.exist?(@path)
+        warn("File not found: #{@path}")
+        exit(1)
+      end
+    end
+
+    def call
+      @excel = Roo::Spreadsheet.open(@path)
+      @excel.sheets.length.times do |i|
+        CSV.open("#{@excel.sheets[i]}.csv", "wb") do |csv|
+          @excel.sheet(i).each { |row| csv << row }
+        end
+      end
+    end
+  end
 end
